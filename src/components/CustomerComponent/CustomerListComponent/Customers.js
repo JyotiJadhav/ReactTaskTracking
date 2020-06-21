b6180632ac2c43164e8db075f92faba49835738c
Jyoti Jadhav <jadhavjyotic@gmail.com>
2020-06-21 22:44:57 +0530

    UI Fixes

--- src/components/CustomerComponent/CustomerListComponent/Customers.js
+++ src/components/CustomerComponent/CustomerListComponent/Customers.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import home from '../home.png'
+import home1 from '../../img/home1.png';
+import DGS from '../../img/DGS.png';
+import bauhaus from '../../img/bauhaus.png';
+import staples1 from '../../img/staples1.png';
+import DHL1 from '../../img/DHL1.png';
+import tshirt from '../../img/tshirt.png';
+
+// Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
 class Customers extends Component {
     render() {
         return (
-            <div className="column">
+            <div className="column padding-left-5">
                 <button className="button button-full-round box-shadow-4p-8p">
-                    <img src={home} alt="Avatar"></img>
+                    <img src={home1} alt="Avatar"></img>
                 </button>
                 <button className="button button-full-round box-shadow-4p-8p">
-                    <img src={home} alt="Avatar"></img>
+                    <img src={DGS} alt="Avatar"></img>
                 </button>
                 <button className="button button-full-round box-shadow-4p-8p">
-                    <img src={home} alt="Avatar"></img>
+                    <img src={bauhaus} alt="Avatar"></img>
                 </button>
                 <button className="button button-full-round box-shadow-4p-8p">
-                    <img src={home} alt="Avatar"></img>
+                    <img src={staples1} alt="Avatar"></img>
+                </button>
+                <button className="button button-full-round box-shadow-4p-8p">
+                    <img src={DHL1} alt="Avatar"></img>
+                </button>
+                <button className="button button-full-round box-shadow-4p-8p">
+                    <img src={tshirt} alt="Avatar"></img>
                 </button>
             </div>
         )
--- src/components/CustomerComponent/OverviewComponent/Overview.js
+++ src/components/CustomerComponent/OverviewComponent/Overview.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Overview extends Component {
     render() {
         return (
-            <button className="button-overview box-shadow-8p-16p">Shadow Button</button>
+            <button className="button-overview box-shadow-8p-16p">Show Details</button>
         )
     }
 }
